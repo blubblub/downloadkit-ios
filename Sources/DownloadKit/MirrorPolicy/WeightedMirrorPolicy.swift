926becf623753cdc1388b65f87d6813d11b56155
Jure Lajlar <jlajlar@gmail.com>
2021-03-26 16:06:40 +0100

    Mirror policy stops retrying if download is canceled.

--- Sources/DownloadKit/MirrorPolicy/WeightedMirrorPolicy.swift
+++ Sources/DownloadKit/MirrorPolicy/WeightedMirrorPolicy.swift
@@ -35,6 +35,12 @@ open class WeightedMirrorPolicy: MirrorPolicy {
     }
     
     public func mirror(for asset: AssetFile, lastMirrorSelection: AssetMirrorSelection?, error: Error?) -> AssetMirrorSelection? {
+        
+        // if download was cancelled, no need to retry or return new mirror
+        if (error as? NSError)?.code == NSURLErrorCancelled {
+            return nil
+        }
+        
         let mirrors = sortMirrors(for: asset)
         
         var selectedIndex = 0
