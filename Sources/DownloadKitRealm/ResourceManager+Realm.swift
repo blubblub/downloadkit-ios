7d101d8c70101c1aa85b5797cb87d34ae2e1430b
Dal Rupnik <legoless@gmail.com>
2025-07-01 13:48:52 +0200

    Update README with corrected API documentation and DownloadProcessor architecture

---
+++ Sources/DownloadKitRealm/ResourceManager+Realm.swift
@@ -0,0 +1,22 @@
+//
+//  ResourceManager+Realm.swift
+//  DownloadKit
+//
+//  Created by Dal Rupnik on 01.07.2025.
+//
+
+import RealmSwift
+import DownloadKitCore
+
+public extension ResourceManager {
+    static func `default`(with configuration: Realm.Configuration = .defaultConfiguration) async -> ResourceManager {
+        let cache = RealmCacheManager<CachedLocalFile>(memoryCache: RealmMemoryCache(configuration: configuration), localCache: RealmLocalCacheManager(configuration: configuration))
+        
+        let downloadQueue = DownloadQueue()
+        await downloadQueue.add(processor: WebDownloadProcessor())
+        
+        let resourceManager = ResourceManager(cache: cache, downloadQueue: downloadQueue)
+                
+        return resourceManager
+    }    
+}
